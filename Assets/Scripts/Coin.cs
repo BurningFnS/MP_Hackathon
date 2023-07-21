17d1cd43ccfc519e98120d14567032bd283359da
AwanCute <117134768+AwanCute@users.noreply.github.com>
2023-07-21 11:25:44 +0800

    Magnet (Working Script)

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public Transform playerTransform;
-    public float moveSpeed = 17f;
-    public static bool coinMove = false;
-
     void Update()
     {
         transform.Rotate(0, 0, 50 * Time.deltaTime);
-
-        if(coinMove == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,11 +15,6 @@ public class Coin : MonoBehaviour
         {
             PlayerManager.numberOfCoins += 1;
             Destroy(gameObject);
-        }
-
-        if (other.tag == "Coin Detector")
-        {
-            coinMove = true;
         }
     }
 }
