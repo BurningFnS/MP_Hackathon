38c344e6d773551514f842e8c45c7054fb316676
JoycelynFoo <83127756+JoycelynFoo@users.noreply.github.com>
2023-08-10 11:57:55 +0800

    JobPagesBeforeRun

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
     }
 }
