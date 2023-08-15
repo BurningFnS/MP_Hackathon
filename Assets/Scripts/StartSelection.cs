ab176e43607623c94aed58d0d0606c481bb4922a
aeson9 <aesonaw143@gmail.com>
2023-08-15 15:08:18 +0800

    Events-UI

---
+++ Assets/Scripts/StartSelection.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSelection : MonoBehaviour
+{
+    public GameObject[] jobPanels;
+    private int currentPanelIndex = 0;
+
+    public void LoadScene(string name)
+    {
+
+        SceneManager.LoadScene(name);
+    }
+
+    //----
+
+    private void Start()
+    {
+        ShowPanel(currentPanelIndex);
+    }
+
+    public void ShowNextPanel()
+    {
+        currentPanelIndex = (currentPanelIndex + 1) % jobPanels.Length;
+        ShowPanel(currentPanelIndex);
+    }
+
+    public void ShowPreviousPanel()
+    {
+        currentPanelIndex = (currentPanelIndex - 1 + jobPanels.Length) % jobPanels.Length;
+        ShowPanel(currentPanelIndex);
+    }
+
+    private void ShowPanel(int index)
+    {
+        for (int i = 0; i < jobPanels.Length; i++)
+        {
+            jobPanels[i].SetActive(i == index);
+        }
+    }
+}
