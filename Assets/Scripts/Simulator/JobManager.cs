fb4065af0c1c02dc5802f4892212ff265ba4b6f6
aeson9 <aesonaw143@gmail.com>
2023-08-18 16:52:19 +0800

    Did UI for Have Job and Congratulations on Job Panels

--- Assets/Scripts/Simulator/JobManager.cs
+++ Assets/Scripts/Simulator/JobManager.cs
@@ -8,6 +8,11 @@ public class JobManager : MonoBehaviour
     //public int salary;
     //public StartSelection startSelection;
     //public Text coinText;
+    public GameObject HaveJobAlreadyPanel;
+    public GameObject CongratulationsOnJobPanel;
+    public Text CongratsJobText;
+    public Text HaveJobText;
+    public Text PhotographerSummaryText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,26 @@ public class JobManager : MonoBehaviour
     void Update()
     {
 
+    }
+    public void CheckJobButton()
+    {
+        if (PlayerPrefs.GetInt("JobIndex") == 0)
+        {
+            
+            //PhotographerSummaryText.text = "Photographer Closed";
+            Debug.Log("WAAAAAAAAAAAAA");
+
+        }
+        if (PlayerPrefs.GetInt("JobIndex") == 1)
+        {
+            //ZooKeeperSummaryText.text = "ZooKeeper Closed";
+            Debug.Log("BBBBBBBBBBBBB");
+        }
+        if (PlayerPrefs.GetInt("JobIndex") == 2)
+        {
+            //PlumberSummaryText.text = "PlumberClosed";
+            Debug.Log("WAHAHAHAH");
+        }
     }
 
 }
