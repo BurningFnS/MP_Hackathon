4cc69237abefdcd2e5d8c7257fbbc2c861096762
AwanCute <117134768+AwanCute@users.noreply.github.com>
2023-08-18 09:50:32 +0800

    Lose condition

--- Assets/Scripts/Simulator/EventManager.cs
+++ Assets/Scripts/Simulator/EventManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
@@ -9,28 +10,43 @@ public class EventManager : MonoBehaviour
     public int[] livingExpenses;
     public GameObject yourExpenses;
     public CoinManager coinManager;
+    public GameObject LoseScreen;
+    public Text loseText;
 
     void Start()
     {
-        yourExpenses.SetActive(true);
-
         for (int i = 0; i < livingExpenses.Length; i++)
         {
             if(i == 0)
             {
-                livingExpenses[0] = 100;
-                livingExpensesText[0].text = "Water Bill: " + livingExpenses[0];
+                livingExpenses[i] = 100;
+                livingExpensesText[i].text = "Water Bill: " + livingExpenses[0];
             }
             if(i == 1)
             {
-                livingExpenses[1] = 100;
-                livingExpensesText[1].text = "Electrical Bill: " + livingExpenses[1];
+                livingExpenses[i] = 100;
+                livingExpensesText[i].text = "Electrical Bill: " + livingExpenses[1];
             }
             if(i == 2)
             {
-                livingExpenses[2] = livingExpenses[0] + livingExpenses[1];
-                livingExpensesText[2].text = "Total Bill: " + livingExpenses[2];
+                livingExpenses[i] = livingExpenses[0] + livingExpenses[1];
+                livingExpensesText[i].text = "Total Bill: " + livingExpenses[i];
             }
+        }
+
+        if (coinManager.currentCoins >= livingExpenses[2])
+        {
+            yourExpenses.SetActive(true);
+        }
+        else if (coinManager.currentCoins < livingExpenses[2])
+        {
+            LoseScreen.SetActive(true);
+            loseText.text = "You do not have enough to pay you your expenses";
+        }
+        else
+        {
+            LoseScreen.SetActive(true);
+            loseText.text = "You have $0 in your\n savings, you do not have enough to live";
         }
     }
 
@@ -41,4 +57,8 @@ public class EventManager : MonoBehaviour
         yourExpenses.SetActive(false);
     }
 
+    public void Restart()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
