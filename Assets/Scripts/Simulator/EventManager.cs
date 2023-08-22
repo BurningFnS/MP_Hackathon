0b35bdf7c04ada34fd6460770ac865c89f0ba87f
BurningFnS <raqeefrashid01@gmail.com>
2023-08-22 23:34:31 +0800

    Revert "debugging"
    This reverts commit f8a78bab16b59ef697e8dff3a60dd60bca78d246.

--- Assets/Scripts/Simulator/EventManager.cs
+++ Assets/Scripts/Simulator/EventManager.cs
@@ -19,7 +19,7 @@ public class EventManager : MonoBehaviour
         int waterBill = PlayerPrefs.GetInt("WaterBill");
         int elecBill = PlayerPrefs.GetInt("ElectricalBill");
 
-        Debug.Log("this shld only play once");
+
         for (int i = 0; i < livingExpenses.Length; i++)
         {
             int changeInCostOfLiving = Random.Range(-50, 100);
@@ -44,7 +44,6 @@ public class EventManager : MonoBehaviour
 
         //Show expenses panel
         yourExpenses.SetActive(true);
-        Debug.Log("reopen expenses");
       
         BuildingClickHandler.canClickOnBuildings = false;
     }
@@ -54,17 +53,9 @@ public class EventManager : MonoBehaviour
         coinManager.currentCoins -= livingExpenses[2];
         coinManager.UpdateCoinDisplay();
         yourExpenses.SetActive(false);
-        Debug.Log("Closing yourExpenses panel");
-        //BuildingClickHandler.canClickOnBuildings = true;
+        BuildingClickHandler.canClickOnBuildings = true;
 
-        OpenRandomEvent();
-    }
-
-    public void OpenRandomEvent()
-    {
-        int randomizedIndex = Random.Range(0, randomEventPanel.Length);
-        Debug.Log($"Opening random event panel at index {randomizedIndex}");
-        randomEventPanel[randomizedIndex].SetActive(true);
+        //randomEventPanel[Random.Range(0, randomEventPanel.Length)].SetActive(true);
     }
 
     public void Restart()
