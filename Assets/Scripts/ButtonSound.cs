83fa9ecbec919c5afe9a4d2a96647c853179942f
JoycelynFoo <83127756+JoycelynFoo@users.noreply.github.com>
2023-08-30 03:45:10 +0800

    AudioMenu

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource soundSource; // Reference to the AudioSource component
+    public AudioClip buttonSound;   // The sound clip to play
+
+    private bool soundPlaying;       // Flag to track if sound is currently playing
+
+    private void Awake()
+    {
+        soundSource = GetComponent<AudioSource>();
+        soundSource.clip = buttonSound;
+    }
+
+    public void PlayButtonSound()
+    {
+        if (!soundPlaying)
+        {
+            soundPlaying = true;
+            soundSource.Play();
+            soundPlaying = false;
+        }
+    }
+
+
+    public void PlayButtonSoundAndLoadScene()
+    {
+        if (!soundPlaying)
+        {
+            soundPlaying = true;
+            soundSource.Play();
+            soundPlaying = false;
+            Invoke("LoadSelectionScene", buttonSound.length); // Invoke LoadScene after sound duration
+        }
+    }
+
+    private void LoadSelectionScene()
+    {
+        SceneManager.LoadScene("Selection");
+    }
+}
